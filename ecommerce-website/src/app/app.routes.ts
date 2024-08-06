1fcbddbcae7ed433ef0a189f15fc3eb71fb5266c
viviantran210 <viviantran210@gmail.com>
2024-08-06 17:53:18 -0400

    fixed routing and added header

--- ecommerce-website/src/app/app.routes.ts
+++ ecommerce-website/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CategoriesComponent } from './components/categories/categories.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', redirectTo: '/home', pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'categories',
+    component: CategoriesComponent
+  },
+];
