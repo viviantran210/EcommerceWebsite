e758a61ca1c62396a432c78b184cc4eef275dbd5
viviantran210 <viviantran210@gmail.com>
2024-08-17 23:29:44 -0400

    added Angular Material menu

--- ecommerce-website/src/app/components/header/header.component.ts
+++ ecommerce-website/src/app/components/header/header.component.ts
@@ -1,18 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
-
+import { Router } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    RouterLink,
+    CommonModule,
     FormsModule,
+    MatMenuModule,
+    MatButtonModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   searchQuery: string = '';
+
+  categories = ["Figures", "Plushies", "Clothes"]
+
+  constructor(private router: Router) {}
+
+  public navigateToCategory(category: string) {
+    this.router.navigate([category.toLowerCase()]);
+  }
 }
