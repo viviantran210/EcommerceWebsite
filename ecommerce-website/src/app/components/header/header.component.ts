79bfe6a4996492bb2319298b07a69e60cba43a88
viviantran210 <viviantran210@gmail.com>
2024-08-24 12:03:17 -0400

    added cart service and styling to cart icon

--- ecommerce-website/src/app/components/header/header.component.ts
+++ ecommerce-website/src/app/components/header/header.component.ts
@@ -1,9 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Subscription } from 'rxjs';
+import { CartService } from '../../service/cart/cart.service';
+
 
 @Component({
   selector: 'app-header',
@@ -13,19 +17,37 @@ import { MatButtonModule } from '@angular/material/button';
     FormsModule,
     RouterLink,
     MatMenuModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy{
   searchQuery: string = '';
+  cartItemCount = 0;
+  private cartSub!: Subscription;
 
   @Input() categories: any[] = []
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private cartService: CartService
+  ) {}
+
+  ngOnInit() {
+    this.cartSub = this.cartService.cartItems$.subscribe(items => {
+      this.cartItemCount = items.reduce((total, item) => total + item.quantity, 0);
+    });
+  }
 
   public navigateToCategory(categoryName: string) {
     this.router.navigate([categoryName.toLowerCase()]);
+  }
+
+  ngOnDestroy(): void {
+    if (this.cartSub) {
+      this.cartSub.unsubscribe();
+    }
   }
 }
