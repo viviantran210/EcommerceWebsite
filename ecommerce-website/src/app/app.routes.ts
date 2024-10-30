4c3771ea29e94f4216c8c999b43f16207f81ce71
viviantran210 <viviantran210@gmail.com>
2024-10-29 22:17:43 -0400

    removed unneeded imports

--- ecommerce-website/src/app/app.routes.ts
+++ ecommerce-website/src/app/app.routes.ts
@@ -1,12 +1,4 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { FiguresComponent } from './components/categories/figures/figures.component';
-import { PlushiesComponent } from './components/categories/plushies/plushies.component';
-import { ClothesComponent } from './components/categories/clothes/clothes.component';
-import { ProductDetailsComponent } from './components/product-details/product-details.component'
-import { ErrorComponent } from './components/error/error.component';
 
 export const routes: Routes = [
   {
