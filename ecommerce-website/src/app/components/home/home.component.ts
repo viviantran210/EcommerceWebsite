12ac6ff62324f2a0892185aaca11b3374f67850c
viviantran210 <viviantran210@gmail.com>
2024-10-30 00:34:55 -0400

    added home page

--- ecommerce-website/src/app/components/home/home.component.ts
+++ ecommerce-website/src/app/components/home/home.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { ProductService } from '../../service/product/product.service';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatTabsModule,
+    MatIconModule,
+    RouterLink,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  categories: any[] = []
 
+  constructor(
+    private productService: ProductService,
+    private router: Router) {}
+
+  ngOnInit() {
+    this.categories = this.productService.getCategories();
+    console.log(this.categories);
+  }
+
+  public navigateToCategory(categoryName: string) {
+    this.router.navigate([categoryName.toLowerCase()]);
+  }
+
+  public goToShop() {
+    this.navigateToCategory("all");
+  }
 }
